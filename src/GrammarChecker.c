d636c30a310a2470cd622270cb87777a530b5a89
Nicholas Sala <beren.14@proton.me>
2025-12-05 16:59:32 +1300

    Tests

--- src/GrammarChecker.c
+++ src/GrammarChecker.c
@@ -39,15 +39,22 @@ bool isVariableOrNumber(enum TokenType type) {
 }
 
 bool isOperator(enum TokenType type) {
-    return type == PLUS || type == MINUS;
+    return type == PLUS || type == MINUS || type == TIMES || type == DIVIDE;
 }
 
-//Check the order in between variables/numbers and operators
+bool isEquals(enum TokenType type) {
+    return type == EQUALS;
+}
+
+//Check the order in between variables/numbers and operators. Two variables/numbers or two operators can't be one after the other.
+// Not valid examples:
+//  1. + * 9 = 10
+//  2. 4x 7 = 2
 bool hasCorrectOrder(struct Token *tokens) {
     struct Token *cursor = tokens;
 
     while (cursor->type != END) {
-        if (cursor->type != EQUALS && (cursor + 1)->type != EQUALS) {
+        if (!isEquals(cursor->type) && !isEquals((cursor + 1)->type)) {
             if (isVariableOrNumber(cursor->type) && isVariableOrNumber((cursor + 1)->type)) return false;
             if (isOperator(cursor->type) && isOperator((cursor + 1)->type)) return false;
         }
@@ -56,6 +63,22 @@ bool hasCorrectOrder(struct Token *tokens) {
     }
 
     return true;
+}
+
+bool beginsWithTimes(struct Token *tokens) {
+    return tokens->type == TIMES;
+}
+
+bool beginsWithDivide(struct Token *tokens) {
+    return tokens->type == DIVIDE;
+}
+
+//TODO refactor: having a struct with tokens and a len field could help here in knowing how to increment the cursor in order to reach the last token
+bool endsWithOperator(struct Token *tokens) {
+    // struct Token *cursor = tokens;
+    // while (cursor->type != END) cursor++;
+    // return isOperator((cursor - 1)->type);
+    return false;
 }
 
 struct GrammarCheckResult checkGrammar(struct Token *tokens) {
@@ -67,6 +90,15 @@ struct GrammarCheckResult checkGrammar(struct Token *tokens) {
 
     if (!hasCorrectOrder(tokens))
         return (struct GrammarCheckResult) { false, "Wrong order in between variables/numbers and operators" };
+
+    if (beginsWithTimes(tokens))
+        return (struct GrammarCheckResult) { false, "Equation can't begin with times" };
+
+    if (beginsWithDivide(tokens))
+        return (struct GrammarCheckResult) { false, "Equation can't begin with divide" };
+
+    if (endsWithOperator(tokens))
+        return (struct GrammarCheckResult) { false, "Equation can't end with operator" };
 
     return (struct GrammarCheckResult) { true, "" };
 }
